9d68b5da27e37d296e5875b7b8ef9f724073508f
kensk8er <kensk8er1017@gmail.com>
2014-03-18 02:38:04 +0000

    edit Svd, change name into SvdSimilarity.py

--- src/SvdSimilarity.py
+++ src/SvdSimilarity.py
@@ -6,6 +6,7 @@ from scipy.sparse import csr_matrix
 from scipy.sparse import vstack
 from scipy.linalg import diagsvd
 from numpy import matrix
+import numpy as np
 
 __author__ = 'kensk8er'
 
@@ -16,9 +17,10 @@ if __name__ == '__main__':
     # hyper parameters
     dimension = 200
 
-    X_train = unpickle('data/train_tfidf.pkl')
+    print 'load data...'
+    X_train = unpickle('data/train_min_tfidf.pkl')
     train_len, train_feature_len = X_train.get_shape()
-    X_test = unpickle('data/test_tfidf.pkl')
+    X_test = unpickle('data/test_min_tfidf.pkl')
     test_len, test_feature_len = X_test.get_shape()
 
     print 'reshape the matrix...'
@@ -36,15 +38,29 @@ if __name__ == '__main__':
     S = matrix(diagsvd(Sigma, dimension, dimension))  # dimension * dimension
     V = matrix(V[:, :dimension])  # dimension * dimension
     X = U * S  # doc_len * dimension
-    X = X * V
-
     X_train, X_test, c = np.vsplit(X, np.array([train_len, train_len + test_len]))
-    X_train = csr_matrix(X_train)
-    X_test = csr_matrix(X_test)
+    X_train = matrix(X_train)
+    X_test = matrix(X_test)
 
     print 'save document matrix...'
-    enpickle(X_train, 'data/train_svd.pkl')
-    enpickle(X_test, 'data/test_svd.pkl')
+    enpickle(X_train, 'data/train_min_svd.pkl')
+    enpickle(X_test, 'data/test_min_svd.pkl')
+
+    # calculate similarity
+    print 'calculate inner products between documents...'
+    inner_products = X_train * X_test.transpose()
+    print 'calculate norm for train data...'
+    train_norm = np.sqrt(np.multiply(X_train, X_train).sum(axis=1))
+    print 'calculate norm for test data...'
+    test_norm = np.sqrt(np.multiply(X_test, X_test).sum(axis=1))
+    print 'calculate norm matrix...'
+    norm_matrix = train_norm * test_norm.transpose()
+    print 'calculate cosine similarities...'
+    similarities = inner_products / norm_matrix
+
+    # save similarity
+    print 'saving similarities...'
+    enpickle(similarities, 'data/similarity_min_svd.pkl')
 
     print 'done!'
 
